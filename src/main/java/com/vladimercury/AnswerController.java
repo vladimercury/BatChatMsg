eeca7a37f0857f17a1938500e1cca4852108bc4c
Vladimir Kuriy <kuriycss@mail.ru>
2016-06-14 04:31:11 +0300

    +history

--- src/main/java/com/vladimercury/AnswerController.java
+++ src/main/java/com/vladimercury/AnswerController.java
@@ -5,15 +5,22 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by vladimercury on 6/13/16.
  */
 @Controller
 public class AnswerController {
+    private static List<Answer> history = new ArrayList<Answer>();
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Answer answer(Message message, SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws Exception{
-        return new Answer(simpMessageHeaderAccessor.getSessionId() + ": " +  message.getContent() + "!");
+        Answer ans = new Answer(simpMessageHeaderAccessor.getSessionId() + ": " +  message.getContent() + "!");
+        history.add(ans);
+        System.out.println(history.size());
+        return ans;
     }
 }
