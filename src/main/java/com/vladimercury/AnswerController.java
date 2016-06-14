394b0098e0ab4bafbcc168f928a98bfdba534e67
Vladimir Kuriy <kuriycss@mail.ru>
2016-06-14 23:42:39 +0300

    to rooms

--- src/main/java/com/vladimercury/AnswerController.java
+++ src/main/java/com/vladimercury/AnswerController.java
@@ -1,19 +1,29 @@
 package com.vladimercury;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vladimercury on 6/13/16.
  */
 @Controller
 public class AnswerController {
-    private static List<Answer> history = new ArrayList<Answer>();
+    private static Map<String, List<Answer>> history = new HashMap<String, List<Answer>>();
+    private SimpMessageSendingOperations messageSendingOperations;
+
+    @Autowired
+    public AnswerController(SimpMessageSendingOperations messageSendingOperations){
+        this.messageSendingOperations = messageSendingOperations;
+    }
 
     private static String tagScreening(String str){
         str = str.replace("&", "&amp;");
@@ -22,25 +32,31 @@ public class AnswerController {
         return str;
     }
 
-    @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Answer answer(Message message, SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws Exception{
+    @MessageMapping("/hello/{roomId}")
+    public void answer(@DestinationVariable String roomId, Message message, SimpMessageHeaderAccessor simpMessageHeaderAccessor) throws Exception{
         Answer ans = new Answer(simpMessageHeaderAccessor.getSessionId(), tagScreening(message.getContent()));
-        history.add(ans);
-        return ans;
+        if (!history.containsKey(roomId)){
+            history.put(roomId, new ArrayList<Answer>());
+        }
+        history.get(roomId).add(ans);
+        messageSendingOperations.convertAndSend("/topic/greetings/" + roomId, ans);
     }
 
-    @MessageMapping("/history")
-    @SendTo("/topic/history")
-    public History getHistory() throws Exception{
-        String[] authors = new String[history.size()];
-        String[] messages = new String[history.size()];
+    @MessageMapping("/history/{roomId}")
+    public void getHistory(@DestinationVariable String roomId) throws Exception{
+        if (!history.containsKey(roomId)){
+            history.put(roomId, new ArrayList<Answer>());
+        }
+        List<Answer> thisChat = history.get(roomId);
+        String[] authors = new String[thisChat.size()];
+        String[] messages = new String[thisChat.size()];
         int index = 0;
-        for (Answer answer : history) {
+
+        for (Answer answer : thisChat) {
             authors[index] = answer.getAuthor();
             messages[index] = answer.getContent();
             index++;
         }
-        return new History(authors, messages);
+        messageSendingOperations.convertAndSend("/topic/history/" + roomId, new History(authors, messages));
     }
 }
